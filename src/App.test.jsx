05065de432680cad814acf9f48c9fcda9db496ab
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-16 21:34:42 +0300

    Test: Render Successfully Fetched Data

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,2 +1,25 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
 
+describe('Data fetching works correctly', () => {
+  let mockedFetch
+  beforeEach(() => {
+    mockedFetch = vi.fn()
+    vi.stubGlobal('fetch', mockedFetch)
+    mockedFetch.mockResolvedValueOnce({
+      status: 200,
+      json: async () => [{ id: 1, title: 'Test Product 1'}]
+    })
+  })
+  afterEach(() => {
+    vi.unstubAllGlobals()
+  })
+
+  it('Renders successfully fetched data', async () => {
+    render(<App />)
+    await waitFor(() => {
+      expect(screen.getByText('Test Product 1')).toBeInTheDocument()
+    })
+  })
+})

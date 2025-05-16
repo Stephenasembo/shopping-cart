827e88acee0d4562a16bb62f0091ae2d48218879
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-16 21:45:57 +0300

    Feat: Implement Loading Screen
    Improves user experience by providing more information to the user
    on what is happening

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [productDetails, setProductDetails] = useState([])
-  console.log(productDetails)
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     (async () => {
       let productResponse = await fetch('https://fakestoreapi.com/products', { mode: 'cors' })
       if (productResponse.status === 200) {
         let products = await productResponse.json();
         setProductDetails(products)
+        setLoading(false)
       }
     })()
   }, [])
 
   return (
     <div>
-      {productDetails.length > 0 && 
+      {
+        loading &&
+        <div>
+          Hang tight while we fetch the latest products.
+        </div>
+      }
+      {productDetails.length > 0 &&
       <ul>
         {productDetails.map((product) => <li key={product.id}>{product.title}</li>)}
       </ul>}

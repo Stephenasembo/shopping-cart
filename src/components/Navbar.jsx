f329283acf0c8fda50b32390f9a8e6955579eb3e
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-05-17 18:18:43 +0300

    Feat: Implement React Routing
    Improves website performance since it now acts as a single page application

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function NavigationBar({addedProducts = []}) {
   return (
     <div>
-      <a href="../App.jsx">Home</a>
-      <a href="./Cart.jsx">Cart</a>
+      <Link to="/">Home</Link>
+      <Link to="/cart">Cart</Link>
       {addedProducts.length > 0 && 
         <div>
           Products added to cart: {addedProducts.length}
